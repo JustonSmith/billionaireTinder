0b2c1027ff58f3f70ad9146ce9ead698f0a30ce1
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-11-10 02:25:23 -0600

    tinderClone

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import TinderCards from './TinderCards';
+import SwipeButtons from './SwipeButtons.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    <TinderCards />
+    <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
